a81eba2d7095babc5740e61f431aab355c682727
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-06 21:32:40 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   eslint: {
-    // Disabling on production builds because we're running checks on PRs via GitHub Actions.
+    // production builds because we're running checks on PRs via GitHub Actions.
     ignoreDuringBuilds: true
   },
   experimental: {
